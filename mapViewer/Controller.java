ca05fa923eed2a32953e93a72d60d45fac7b96a0
YC <yclai97@gmail.com>
2018-12-10 23:37:22 +0800

    improve the code structure to increase readability

--- mapViewer/Controller.java
+++ mapViewer/Controller.java
@@ -6,9 +6,9 @@ import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 
 import javafx.scene.layout.RowConstraints;
-import application.Main;
 
 public class Controller {
+	MapViewerModel model = new MapViewerModel();
 	TileMapMV tilemap = new TileMapMV(16);
     public void boatBtnClicked(){
         System.out.println("testing");
@@ -26,30 +26,8 @@ public class Controller {
     
     @FXML
     public void initialize() {
-		
-		for (int i = 0; i < 40; i++) {
-			ColumnConstraints colConstraints = new ColumnConstraints();
-			colConstraints.setPercentWidth(2.5);
-			grid.getColumnConstraints().add(colConstraints);
-		}
 
-		for (int i = 0; i < 40; i++) {
-			RowConstraints rowConstraints = new RowConstraints();
-			rowConstraints.setPercentHeight(2.5);
-			grid.getRowConstraints().add(rowConstraints);
-		}
-
-		tilemap.loadTiles("/Tilesets/testtileset.gif");
-		tilemap.loadMap("/Maps/testmap.map");
-				
-		for (int row = 0; row < 40; row++) {
-			for (int col = 0; col < 40; col++) {
-				
-				tilemap.GenerateTileImage(grid, row, col);
-			}
-
-		}
-		
+		model.loadMap(grid);
 
 }
 }
