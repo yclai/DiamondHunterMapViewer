000913c2fec5c2f43770697a5da6b5b3231ee873
zcwong <32235937+zcwong@users.noreply.github.com>
2018-12-10 16:15:30 +0800

    Add files via upload

---
+++ mapViewer/Controller.java
@@ -0,0 +1,17 @@
+package sample;
+import
+
+public class Controller {
+
+    public void boatBtnClicked(){
+        System.out.println("testing");
+        setBoat(); /*   to set boat on map when button clicked      */
+    }
+
+
+    public void axeBtnClicked(){
+        System.out.println("testing2");
+        setAxe(); /*   to set axe on map when button clicked      */
+    }
+
+}
