ec5a291ea174935cf54caa9cd768b9aeff4c0770
YC <yclai97@gmail.com>
2018-12-13 12:59:05 +0800

    merging

--- DiamondHunter_MapViewer/src/MapViewer/MapViewerModel.java
+++ DiamondHunter_MapViewer/src/MapViewer/MapViewerModel.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Alert.AlertType;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
-
-
 import java.io.File;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
